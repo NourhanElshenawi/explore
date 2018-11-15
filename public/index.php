48c86a64edd12c251efa9321711b82b8d4007de4
Nourhan Elshenawi <n.elshenawi@acg.edu>
2018-11-15 01:04:03 +0000

    Template: Basic Workings

---
+++ public/index.php
@@ -0,0 +1,35 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../app/recaptchalib.php';
+
+use Dotenv\Dotenv;
+use Nourhan\Controllers;
+use Nourhan\Router;
+
+//Load .env
+$dotenv = new Dotenv($path = __DIR__ . '/..');
+if (file_exists($path . '/.env'))
+{
+    $dotenv->load();
+    $dotenv->required(
+        [
+            'CLEARDB_DATABASE_URL',
+        ]
+    )->notEmpty();
+}
+
+
+$router = new Router\Router();
+
+
+
+/** PUBLIC **/
+$router->get('/', 'MainController', 'index');
+
+
+////See inside $router
+//echo "<pre>";
+//print_r($router);
+
+$router->submit();
